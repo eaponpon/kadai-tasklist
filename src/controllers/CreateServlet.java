1ba45239a7b14604e40cc7ef64899fc98bbc3593
五條篤次 <eaponponpon@gmail.com>
2022-10-16 17:52:04 +0900

    「フラッシュメッセージを追加」

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -48,8 +48,10 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
+
             em.persist(m);
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
