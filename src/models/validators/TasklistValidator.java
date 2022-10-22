2d18872c4b407dd01300796f3da9c56cb2e83e0b
五條篤次 <eaponponpon@gmail.com>
2022-10-22 09:51:33 +0900

    「メッセージの修正を追加」

--- src/models/validators/TasklistValidator.java
+++ src/models/validators/TasklistValidator.java
@@ -25,7 +25,7 @@ public class TasklistValidator {
     // メッセージの必須入力チェック
     private static String validateContent(String content) {
         if(content == null || content.equals("")) {
-            return "メッセージを入力してください。";
+            return "タスクを入力してください。";
         }
 
         return "";
